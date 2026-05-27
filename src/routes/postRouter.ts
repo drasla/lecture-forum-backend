b9293e303f82757d52ada8a6ce4c45619ab7577b
Tae.Garam (Office) <drasla217@gmail.com>
2026-05-27 16:59:36 +0900

    postController.getPostsByCategory 생성

---
+++ src/routes/postRouter.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import postController from "../controllers/postController.ts";
+
+const router = Router();
+
+
+router.get("/list/:categoryId", postController.getPostsByCategory);
+
+export default router;
